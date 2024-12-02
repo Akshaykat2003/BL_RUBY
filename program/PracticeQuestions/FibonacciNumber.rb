a6a517551231980072135a295b3e16b30c852741
akshay <akshaykatoch38@gmail.com>
2024-12-02 10:08:43 +0530

    Initial comments

--- program/PracticeQuestions/FibonacciNumber.rb
+++
@@ -1,9 +0,0 @@
-# Input
-n = gets.to_i
-
-# Compute Fibonacci
-fib = [0, 1]
-(2..n).each { fib << fib[-1] + fib[-2] }
-
-# Output result
-puts fib[n]
--- program/PracticeQuestions/TribonacciNumber.rb
+++
@@ -1,9 +0,0 @@
-# Input
-n = gets.to_i
-
-# Compute Tribonacci
-trib = [0, 1, 1]
-(3..n).each { trib << trib[-1] + trib[-2] + trib[-3] }
-
-# Output result
-puts trib[n]
